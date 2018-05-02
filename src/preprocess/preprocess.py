ee6baad39ae8178bd2925fee4aff5adda06aa59b
Martin Gruenbaum <martin@itsolveonline.net>
2018-05-02 16:29:04 +0200

    Fix incorrect default filepaths for preprocess.py

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -7,9 +7,9 @@ import codecs
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_txt', default='data/tiny-shakespeare.txt')
-parser.add_argument('--output_h5', default='data/tiny-shakespeare.h5')
-parser.add_argument('--output_json', default='data/tiny-shakespeare.json')
+parser.add_argument('--input_txt', default='/data/tiny-shakespeare.txt')
+parser.add_argument('--output_h5', default='/data/tiny-shakespeare.h5')
+parser.add_argument('--output_json', default='/data/tiny-shakespeare.json')
 parser.add_argument('--val_frac', type=float, default=0.1)
 parser.add_argument('--test_frac', type=float, default=0.1)
 parser.add_argument('--quiet', action='store_true')
